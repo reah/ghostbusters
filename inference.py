7fa8f323503ec49fcf3da191dfc10410a6c7c594
Alex Kern <alex@kern.io>
2014-03-18 00:22:14 -0700

    This project is #OVER.

--- inference.py
+++ inference.py
@@ -528,6 +528,10 @@ class JointParticleFilter:
 
             "*** YOUR CODE HERE ***"
 
+            for i in range(self.numGhosts):
+                newPosDist = getPositionDistributionForGhost(setGhostPositions(gameState, oldParticle), i, self.ghostAgents[i])
+                newParticle[i] = util.sample(newPosDist)
+
             "*** END YOUR CODE HERE ***"
             newParticles.append(tuple(newParticle))
         self.particles = newParticles
